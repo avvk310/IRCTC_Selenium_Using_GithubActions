51274b24bea4f55c36da1e238857ab0a71c846b7
avvk310 <vivekavk396@gmail.com>
2025-11-15 21:16:10 +0530

    First commit with Action

--- capstone-irctc/conftest.py
+++ capstone-irctc/conftest.py
@@ -1,35 +1,35 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-import time
-import os
 
 @pytest.fixture(scope="class")
 def setup(request):
     chrome_options = Options()
 
-    # ❌ DO NOT USE HEADLESS (IRCTC WILL NOT LOAD)
+    # IMPORTANT: DO NOT USE HEADLESS (IRCTC blocks headless Chrome)
     # chrome_options.add_argument("--headless")
 
-    # Tell Chrome to use the Xvfb virtual display
-    chrome_options.add_argument("--display=:99")
+    # Xvfb already sets DISPLAY=:99 in GitHub Actions
+    # DO NOT ADD chrome_options.add_argument("--display=:99")
 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
 
-    # IRCTC specific stabilization
+    # Anti-bot detection bypass
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-popup-blocking")
 
+    # Start Chrome
     driver = webdriver.Chrome(options=chrome_options)
     wait = WebDriverWait(driver, 40)
 
+    # IRCTC loads slowly in GitHub Actions → require extra time
     driver.get("https://www.irctc.co.in/nget/train-search")
-
     time.sleep(10)
 
     request.cls.driver = driver
