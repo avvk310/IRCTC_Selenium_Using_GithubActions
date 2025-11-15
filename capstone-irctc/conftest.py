9cdf979eb748b86f6618fdbb4c3aa4d6c1772744
avvk310 <vivekavk396@gmail.com>
2025-11-15 19:59:38 +0530

    First commit with Action

--- capstone-irctc/conftest.py
+++ capstone-irctc/conftest.py
@@ -3,31 +3,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 import time
+import os
 
 @pytest.fixture(scope="class")
 def setup(request):
     chrome_options = Options()
 
-    # FIX: IRCTC does NOT load properly in --headless=new mode
-    chrome_options.add_argument("--headless=chrome")     # ← use this
+    # ❌ DO NOT USE HEADLESS (IRCTC WILL NOT LOAD)
+    # chrome_options.add_argument("--headless")
+
+    # Tell Chrome to use the Xvfb virtual display
+    chrome_options.add_argument("--display=:99")
+
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
 
-    # IRCTC-specific fixes
+    # IRCTC specific stabilization
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=chrome_options)
     wait = WebDriverWait(driver, 40)
 
     driver.get("https://www.irctc.co.in/nget/train-search")
 
-    # IRCTC takes time to load Angular + popups
-    time.sleep(12)
+    time.sleep(10)
 
     request.cls.driver = driver
     request.cls.wait = wait
